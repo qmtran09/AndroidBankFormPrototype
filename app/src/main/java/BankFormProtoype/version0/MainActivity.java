414a5e54c70202e6c60b1ca8ebb2fb554d0ddbd7
Quang Tran <qmtran09@gmail.com>
2021-06-06 22:00:59 +0700

    Name field form validation

--- app/src/main/java/BankFormProtoype/version0/MainActivity.java
+++ app/src/main/java/BankFormProtoype/version0/MainActivity.java
@@ -7,7 +7,12 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.service.autofill.Validator;
 import android.text.Editable;
+import android.text.InputFilter;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +21,12 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -50,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         mDisplayDate = (TextView) findViewById(R.id.expiryDate);
+
         mCardNum = (EditText) findViewById(R.id.cardNum);
         mNameLabel = (TextView) findViewById(R.id.cardHolderLabel);
         mName = (EditText) findViewById(R.id.cardHolder);
@@ -71,8 +82,10 @@ public class MainActivity extends AppCompatActivity {
         mTypeIDlabel.setVisibility(TextView.INVISIBLE);
         mTypeID.setVisibility(EditText.INVISIBLE);
 
+        //make name all caps
 
-
+        mName.setFilters(new InputFilter[]{getEditTextFilter(),new InputFilter.AllCaps()});
+       // mName.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,9 +98,9 @@ public class MainActivity extends AppCompatActivity {
                         MainActivity.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         mDateSetListener,
-                        year,month,day);
+                        year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.getDatePicker().findViewById(getResources().getIdentifier("day","id","android")).setVisibility(View.GONE);
+                dialog.getDatePicker().findViewById(getResources().getIdentifier("day", "id", "android")).setVisibility(View.GONE);
                 dialog.show();
             }
         });
@@ -102,71 +115,116 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-       mCardNum.addTextChangedListener(new TextWatcher() {
-           @Override
-           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        mCardNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-           }
+            }
 
-           @Override
-           public void onTextChanged(CharSequence s, int start, int before, int count) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-           }
+            }
 
-           @Override
-           public void afterTextChanged(Editable s) {
-               if(s.toString().trim().length() >0){
-                   mNameLabel.setVisibility(TextView.VISIBLE);
-                   mName.setVisibility(EditText.VISIBLE);
-               }
-           }
-       });
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().trim().length() > 0) {
+                    mNameLabel.setVisibility(TextView.VISIBLE);
+                    mName.setVisibility(EditText.VISIBLE);
+                }
+            }
+        });
 
-       mName.addTextChangedListener(new TextWatcher() {
-           @Override
-           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        mName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-           }
+            }
 
-           @Override
-           public void onTextChanged(CharSequence s, int start, int before, int count) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-           }
+            }
 
-           @Override
-           public void afterTextChanged(Editable s) {
-               if(s.toString().trim().length() >0){
-                   mExpiryLabel.setVisibility(TextView.VISIBLE);
-                   mDisplayDate.setVisibility(EditText.VISIBLE);
-                   mCVVlabel.setVisibility(TextView.VISIBLE);
-                   mCVV.setVisibility(EditText.VISIBLE);
-               }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().trim().length() > 0) {
+                    mExpiryLabel.setVisibility(TextView.VISIBLE);
+                    mDisplayDate.setVisibility(EditText.VISIBLE);
+                    mCVVlabel.setVisibility(TextView.VISIBLE);
+                    mCVV.setVisibility(EditText.VISIBLE);
+                }
 
-           }
-       });
+            }
+        });
 
-       mCVV.addTextChangedListener(new TextWatcher() {
-           @Override
-           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        mCVV.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-           }
+            }
 
-           @Override
-           public void onTextChanged(CharSequence s, int start, int before, int count) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-           }
+            }
 
-           @Override
-           public void afterTextChanged(Editable s) {
-               if(s.toString().trim().length() >0){
-                   mSelectId.setVisibility(TextView.VISIBLE);
-                   spinner.setVisibility(EditText.VISIBLE);
-                   mTypeIDlabel.setVisibility(TextView.VISIBLE);
-                   mTypeID.setVisibility(EditText.VISIBLE);
-               }
-           }
-       });
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().trim().length() > 0) {
+                    mSelectId.setVisibility(TextView.VISIBLE);
+                    spinner.setVisibility(EditText.VISIBLE);
+                    mTypeIDlabel.setVisibility(TextView.VISIBLE);
+                    mTypeID.setVisibility(EditText.VISIBLE);
+                }
+            }
+        });
+        mName.addTextChangedListener(new TextValidator(mName) {
+            @Override
+            public void validate(TextView textView, String text) {
+                if(text.contains(" ")){
 
+                }else{
+                    textView.setError("Name must contain a space");
+                }
 
+            }
+        });
+
+    }
+    public static InputFilter getEditTextFilter() {
+        return new InputFilter() {
+
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+                boolean keepOriginal = true;
+                StringBuilder sb = new StringBuilder(end - start);
+                for (int i = start; i < end; i++) {
+                    char c = source.charAt(i);
+                    if (isCharAllowed(c)) // put your condition here
+                        sb.append(c);
+                    else
+                        keepOriginal = false;
+                }
+                if (keepOriginal)
+                    return null;
+                else {
+                    if (source instanceof Spanned) {
+                        SpannableString sp = new SpannableString(sb);
+                        TextUtils.copySpansFrom((Spanned) source, start, sb.length(), null, sp, 0);
+                        return sp;
+                    } else {
+                        return sb;
+                    }
+                }
+            }
+
+            private boolean isCharAllowed(char c) {
+                Pattern ps = Pattern.compile("^[a-zA-Z ]+$");
+                Matcher ms = ps.matcher(String.valueOf(c));
+                return ms.matches();
+            }
+        };
     }
 }
